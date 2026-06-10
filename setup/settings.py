45fd73f8cb6691054582f43b2f16d7a3e480b7fa
maykawlin <maykawlin@hotmail.com>
2026-06-10 16:22:29 -0300

    fix: adiciona dominios oficiais no allowed hosts e cors

--- setup/settings.py
+++ setup/settings.py
@@ -171,6 +171,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",   
     "http://127.0.0.1:5173",
     "https://projeto-educa-beta.vercel.app",
+    "https://materialdidaticos.com.br",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
