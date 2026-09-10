496396af71bf9f5c8bae412b4781e60df6693ced
maykawlin <maykawlin@hotmail.com>
2026-09-10 17:38:37 -0300

    change: altera o texto sobre os didaticos no footer-3

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -10,7 +10,7 @@ export function Footer({ setPaginaAtual }) {
                     <h3>📚 Sobre a Didáticos</h3>
                     <p style={{ textAlign: 'justify' }}>
                         Na Didáticos, acreditamos que o professor merece mais tempo para ensinar e menos tempo preparando materiais.
-                        Criamos atividades, apostilas, slides, e-books e recursos pedagógicos prontos, desenvolvidos com qualidade e 
+                        Criamos atividades, apostilas, slides, e-books e recursos pedagógicos desenvolvidos com qualidade e 
                         organização para facilitar a rotina dos educadores. Nosso compromisso é ser muito mais que um fornecedor de 
                         materiais: queremos ser o parceiro de confiança dos professores.{' '}
                         <span 
